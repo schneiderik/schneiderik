33cdaf26c770b9054c8d94013da743be7488b1e7
Erik Schneider <erik.schneider41@gmail.com>
2014-07-06 18:14:41 -0500

    Revert "Move cart system to tictail"
    This reverts commit cf6d867489cdd75821e5d5abf8aa48a65073fe13.

--- js/main.js
+++ js/main.js
@@ -28,20 +28,62 @@ var toggleContent = function(selector) {
   addClass(targetContent, 'active');
 }
 
-var startsWithHash = /^\#/g;
+var UrlParameters = function () {
+  var query_string = {};
+  var query = window.location.search.substring(1);
+  var vars = query.split("&");
+  for (var i=0;i<vars.length;i++) {
+    var pair = vars[i].split("=");
+    if (typeof query_string[pair[0]] === "undefined") {
+      query_string[pair[0]] = pair[1];
+    } else if (typeof query_string[pair[0]] === "string") {
+      var arr = [ query_string[pair[0]], pair[1] ];
+      query_string[pair[0]] = arr;
+    } else {
+      query_string[pair[0]].push(pair[1]);
+    }
+  }
+  return query_string;
+} ();
 
 var tabClick = function(e) {
-  var href = e.target.getAttribute('href');
-  if (href.match(startsWithHash)) {
-    e.preventDefault();
-    var active_item = document.querySelectorAll('nav.tabs a.active')[0];
-    removeClass(active_item, 'active');
-    addClass(e.target, 'active');
-    toggleContent(href);
-  }
+  e.preventDefault();
+  var active_item = document.querySelectorAll('nav.tabs a.active')[0];
+  removeClass(active_item, 'active');
+  addClass(e.target, 'active');
+  toggleContent(e.target.getAttribute('href'));
+}
+
+var updateCart = function() {
+  var cart = document.querySelectorAll('#cart input[type="submit"], #project-topper input[type="submit"]')[0];
+
+  var itemCount = 0
+  eachItem(paypal.minicart.cart.items(), function(item) {
+    itemCount += item.get('quantity')
+  });
+  cart.value = "Cart (" + itemCount + ")"
+}
+
+var cartAltered = function(idx, product, isExisting) {
+  updateCart();
+
+  product.on('change', updateCart)
 }
 
 var nav_tabs = document.querySelectorAll('nav.tabs a');
 eachItem(nav_tabs, function(item) {
   item.addEventListener('click', tabClick);
 });
+
+// paypal.minicart.render({action: "https://www.sandbox.paypal.com/cgi-bin/webscr"});
+paypal.minicart.render({action: "https://www.paypal.com/cgi-bin/webscr"});
+
+if (UrlParameters.co == 'y') {
+  paypal.minicart.reset();
+}
+
+paypal.minicart.cart.on("add", cartAltered);
+paypal.minicart.cart.on("remove", cartAltered);
+updateCart();
+
+
